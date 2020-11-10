054853ebb97672be135a42328bcd82b703d11332
SeanGau <rrtw0627@gmail.com>
2020-11-10 14:25:07 +0800

    add fanpage numbers

--- crawler.js
+++ crawler.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const request = require('request');
+const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const config = require('./config.json');
 
@@ -24,49 +24,39 @@ if(config.sql) {
 
 const client = new Client(sql_config);
 
-var all_data={"slack": {}, "hackathon": {}, "summit": {"count": 4}};
-
-function craw_slack() {
-  return new Promise(function (resolve, reject) {
-    request(config.slack_api, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var _data = JSON.parse(body);
-        all_data['slack'] = _data;
-        console.log("slack api get!");
-        resolve();
-      }
-      else {
-        console.log("slack api error");
-        reject();
-      }
-    });
-  });
-}
-
-function craw_database() {
-  return new Promise(function (resolve, reject) {
-    request(config.g0v_database, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        //console.log(body);
-        var $ = cheerio.load(body);
-        all_data['hackathon']['proposals'] = $('tr').length - 3;
-        var last_row = $($('tr')[$('tr').length-1]);
-        all_data['hackathon']['count'] = Number($($('td', last_row)[1]).text())+1;
-        all_data['hackathon']['current_title'] = $($('td', last_row)[2]).text();
-        console.log("g0v database get");
-        resolve();
-      }
-      else {
-        console.log("g0v database error");
-        reject();
-      }
-    });
-  });
-}
+var all_data={"slack": {}, "hackathon": {}, "summit": {"count": 4}, "fanpage": {}};
 
 async function a(){
-  await craw_slack();
-  await craw_database();
+  await fetch(config.slack_api)
+  .then(res => res.text())
+  .then(body => {
+    all_data['slack'] = JSON.parse(body);
+    console.log("slack api get!");
+  });
+
+  await fetch(config.g0v_database)
+  .then(res => res.text())
+  .then(body => {
+    var $ = cheerio.load(body);
+    all_data['hackathon']['proposals'] = $('tr').length - 3;
+    var last_row = $($('tr')[$('tr').length-1]);
+    all_data['hackathon']['count'] = Number($($('td', last_row)[1]).text())+1;
+    all_data['hackathon']['current_title'] = $($('td', last_row)[2]).text();
+    console.log("g0v database get!");
+  });
+
+  await fetch(config.g0v_fanpage)
+  .then(res => res.text())
+  .then(body => {
+    var $ = cheerio.load(body);
+    var like = Number($("body").text().match('[0-9,]*(?= 人說這讚)')[0].replace(",",""));
+    var follow = Number($("body").text().match('[0-9,]*(?= 人在追蹤)')[0].replace(",",""));
+    all_data['fanpage'] = {
+      "like": like,
+      "follow": follow
+    }
+    console.log("g0v fanpage get!");
+  });
 }
 
 a().then(() => {
